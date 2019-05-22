826f15714cb9fdd65dfc4ca3dce1285fea48e59f
Abhishek Jha <j7.abhi@gmail.com>
2019-05-22 12:47:36 +0530

    Added tess

--- test.py
+++ test.py
@@ -2,3 +2,32 @@
 todo
 write tests 
 '''
+import numpy as np
+import math
+import os
+from PIL import Image
+def psnr(img1, img2):
+    mse = np.mean( (img1 - img2) ** 2 )
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 255.0
+    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+orig = os.listdir('./dataset/')
+s = 0
+for i in orig:
+    print(i[:-4])
+    psnr_val = psnr(np.asarray(Image.open('dataset/%s'%i)),np.asarray(Image.open('out/decompressed/%scomp.png'%i[:-4])))
+    s+=psnr_val
+    print(' PSNR value is %s'%psnr_val)
+avg = s/len(orig)
+print('Average PSNR ratio is: %s'%avg)
+orig_size = sum(os.path.getsize('dataset/%s'%f) for f in os.listdir('dataset')) 
+comp_size = sum(os.path.getsize('out/compressed/%s'%f) for f in os.listdir('out/compressed/') if '.xfr' in f)
+comp_ratio = orig_size/comp_size
+print(orig_size)
+print(comp_ratio)
+print('Compression ratio is %s'%comp_ratio)
+'''
+psnr 30.41
+factor 14.25
+'''
