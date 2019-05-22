b2888ffb8eaa9c378c54ba7a660d53cf60c92d61
Abhishek Jha <j7.abhi@gmail.com>
2019-05-22 12:24:22 +0530

    update

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ args = parser.parse_args()
 model = Autoencoder().float()
 criterion = nn.SmoothL1Loss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-exp_lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[35,45,55], gamma=0.1)
+exp_lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[40,55,59], gamma=0.1)
 if torch.cuda.is_available():
     model = model.cuda()
 
